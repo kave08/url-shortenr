5dc14e377effb8e1e6bb24bc44c1e84d98f0298a
kave haj <kave.haj@snapp.cab>
2023-02-22 15:36:14 +0330

    added url resolver

--- api/routes/resolve.go
+++ api/routes/resolve.go
@@ -1,1 +1,33 @@
 package routes
+
+import (
+	"url-shortenr/database"
+
+	"github.com/go-redis/redis/v8"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func ResolverURL(c *fiber.Ctx) error {
+	url := c.Params("url")
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	value, err := r.Get(database.Ctx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "short not found in database",
+		})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "can't connect to database",
+		})
+	}
+
+	rInr := database.CreateClient(1)
+	defer rInr.Close()
+	_ = rInr.Incr(database.Ctx, "counter")
+	return c.Redirect(value, 301)
+
+	return nil
+}
