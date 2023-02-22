398dcc5a05652770103bce3387a9b4284c6c0a04
kave haj <kave.haj@snapp.cab>
2023-02-22 21:07:15 +0330

    set data in redis

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -10,6 +10,7 @@ import (
 	"github.com/asaskevich/govalidator"
 	"github.com/go-redis/redis/v8"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 )
 
 type request struct {
@@ -37,6 +38,7 @@ func ShortenURL(c *fiber.Ctx) error {
 	rC := database.CreateClient(1)
 	defer rC.Close()
 	val, err := rC.Get(database.Ctx, c.IP()).Result()
+
 	if err == redis.Nil {
 		_ = rC.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
 	} else {
@@ -65,6 +67,35 @@ func ShortenURL(c *fiber.Ctx) error {
 	//enforce http, SSL
 
 	body.URL = helpers.EnforceHTTP(body.URL)
+
+	var id string
+	if body.CustomShort == "" {
+		id = uuid.New().String()[:6]
+	} else {
+		id = body.CustomShort
+	}
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	val, _ = r.Get(database.Ctx, id).Result()
+	if val != "" {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"error": "URL Custom is already in use",
+		})
+	}
+
+	if body.Exipiry == 0 {
+		body.Exipiry = 24
+	}
+
+	err = r.Set(database.Ctx, id, body.URL, body.Exipiry*3600*time.Second).Err()
+	if err != nil {
+		c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "unable to connect to server",
+		})
+	}
+	rC.Decr(database.Ctx, c.IP())
 
 	return nil
 }
